417b38e6d0477ada05ed8ea1441ac4bb8b0006cd
wkddlstn <wkddlstn1321@naver.com>
2019-04-29 19:28:26 +0900

    네번째 커밋

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@ int main(){
     
     printf("hello");
     printf("commit");
+    printf("git add 파일명");
+    printf("git commit -m "");
+    printf("git push -u origin master");
 
     return 0;
 }
